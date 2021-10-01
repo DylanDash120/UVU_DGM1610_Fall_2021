f6a686229c91ee0820dbf9f1c8015f5762b9b16b
Dylan Clements <10778196@uvu.edu>
2021-10-01 09:50:28 -0600

    Added Variables

---
+++ Class Project/Assets/Scripts/Variables.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string name = "Dylan";
+		Console.WriteLine(name);
+		int myNum = 3;
+		Console.WriteLine(myNum);
+		myNum = 10
+		Console.WriteLine(myNum);
+		long myNum = 13000000000L;
+		Console.WriteLine(myNum);
+		float myNum = 15.69F;
+		Console.WriteLine(myNum);
+		double myNum = 14.26D;
+		Console.WriteLine(myNum);
+		float f1 = 59e5F;
+		double d1 = 45E8D;
+		Console.WriteLine(f1);
+		Console.WriteLine(d1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
