51ffd2f2ca39740f6274ae9cde9209eec21aa14f
Dylan Clements <10778196@uvu.edu>
2021-10-07 15:07:40 -0600

    Added WhileLoops

---
+++ Class Project/Assets/Scripts/WhileLoops.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoops : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 0;
+		while (i<10);
+		{
+			Console.WriteLine(i);
+			i++;
+		}
+	}
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
