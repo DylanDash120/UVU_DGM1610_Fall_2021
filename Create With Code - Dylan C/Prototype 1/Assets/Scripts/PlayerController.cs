77096bd7ceaaae1ed1377a262ae8db7a1c67d1dd
Dylan Clements <10778196@uvu.edu>
2021-12-02 16:23:06 -0700

    Added UnityEventsBehaviour.cs

--- Create With Code - Dylan C/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create With Code - Dylan C/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,19 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Private Variables
-    private float speed = 25.0f;
-    private float turnSpeed = 25f;
+    [SerializeField] private float speed = 25.0f;
+    [SerializeField] private float turnSpeed = 25f;
     private float horizontalInput;
     private float forwardInput;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //This is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
